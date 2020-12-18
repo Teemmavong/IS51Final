f2a53d7dff4ff694a77dbd50874c1188a665b4db
Teemmavong <Teemmavong@gmail.com>
2020-12-17 18:45:04 -0800

    structured-english

---
+++ final_test.py
@@ -0,0 +1,9 @@
+"""
+We want to read in a file
+Capture the number of grades
+The average grade
+Percentage of grades above the average grade
+Exit the file
+Display the correct answers to the user
+"""
+
