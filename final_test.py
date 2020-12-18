6cfb422c0a621eddc3e56368c862ad6a369d90e7
Teemmavong <Teemmavong@gmail.com>
2020-12-17 18:47:01 -0800

    psuedocode

--- final_test.py
+++ final_test.py
@@ -6,4 +6,23 @@ Percentage of grades above the average grade
 Exit the file
 Display the correct answers to the user
 """
+"""
+main
+file = Final.txt
+percentaboveaverage(file)
 
+percentageaboveaverage(file)
+infile
+gradelist[read per line]
+infile.close
+length = len(gradelist)
+sumofgrades = sum(gradelist)
+avg = sumofgrades / length
+print(number of grades)
+print(average)
+count = 0
+if item in gradelist > average
+add 1 to count
+percentaboveavg = count / length
+print percentaboveavg
+"""
