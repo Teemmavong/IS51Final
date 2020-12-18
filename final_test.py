63674f73f992f92790778495746d5bf43991f12f
Teemmavong <Teemmavong@gmail.com>
2020-12-17 19:00:21 -0800

    exam-end

--- final_test.py
+++ final_test.py
@@ -26,3 +26,26 @@ add 1 to count
 percentaboveavg = count / length
 print percentaboveavg
 """
+def main():
+    file = "Final.txt"
+    calculate_percent_above_average(file)
+
+def calculate_percent_above_average(file):
+    infile = open(file, 'r')
+    GradeList = [int(line.rstrip()) for line in infile]
+    infile.close()
+    TotalGrades = len(GradeList)
+    SumOfGrades = sum(GradeList)
+    AverageOfGrades = SumOfGrades / TotalGrades
+    print("The number of grades is: ", TotalGrades)
+    print("The average grade was: ", AverageOfGrades)
+    NumberOfGradesAboveAverage = 0
+    for item in GradeList:
+        if item > AverageOfGrades:
+            NumberOfGradesAboveAverage += 1
+    PercentageAboveAverage = NumberOfGradesAboveAverage / TotalGrades
+    print("Percentage of grades above average was: ", end = " ")
+    print("{0:.2%}".format(PercentageAboveAverage))
+
+
+main()
